ce2ffc6a243a38b4055a784730804ec1b7f8686e
Crista Cooper <aine3d@gmail.com>
2017-11-16 15:06:09 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,7 @@
 
 
 def badge_maker(names)
-  names.each { |name| puts "Hello, my name is #{name}."}
+  conference = []
+  names.each { |name| conference << name}
+  puts "Hello, my name is #{name}."
 end
