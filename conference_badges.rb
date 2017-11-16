f0020a0c31e096359fb1b5d10cc75e8bdebaf607
Crista Cooper <aine3d@gmail.com>
2017-11-16 19:15:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,10 +9,8 @@ def assign_rooms(speakers)
 rooms = [1, 2, 3, 4, 5, 6, 7]
 
 speakers.each_with_index { | speaker |
+  puts rooms[speaker]
 
-rooms[speaker] = room
-
-puts "Hello #{speaker}! You'll be assigned to room #{rooms}"
 
 }
 
